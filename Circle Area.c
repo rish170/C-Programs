360855d14504f69b37d286f34c39a0a42b0ff051
Rishab Thutheja <rishab.17tt@gmail.com>
2024-08-14 11:47:20 +0530

    Circle Area C Program

---
+++ Circle Area.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int rad;
+    printf("Enter Radius of Circle (cm): ");
+    scanf("%d", &rad);
+    printf("Area of Circle= %f", 3.14*rad*rad);
+    return 0;
+}
