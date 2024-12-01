569a72c4d0b3eb7bbd17fe05316e5af4a2efd853
Rishab Thutheja <rishab.17tt@gmail.com>
2024-12-01 16:17:13 +0530

    Lab Program 5

---
+++ Lab Programs/Fibonacci.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int a=0; int b=1;
+    int n; int i = 2;
+    int c;
+    printf("n: ");
+    scanf("%d", &n);
+    printf("%d %d ", a, b);
+    while(i < n){
+        c = a+b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+        i++;
+    }
+    return 0;
+}
