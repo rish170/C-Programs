0e247564beab10ed76626168cd738eb72b63c0f5
Rishab Thutheja <rishab.17tt@gmail.com>
2024-12-01 13:56:08 +0530

    Lab Practice Control Statements 2

---
+++ Practice Programs/Control Statements/Max of three int.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+	int a,b,c;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+
+	if (a>b && a>c){
+		printf("%d\n", a);
+	}
+	else if (b>a && b>c){
+		printf("%d\n", b);
+	}
+	else{
+		printf("%d\n", c);
+	}
+}
